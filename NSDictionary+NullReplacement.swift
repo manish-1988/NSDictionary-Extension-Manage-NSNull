7ffff2038319a3428d7f6fda620dd21a8bc14f40
Manish Pathak <mpathak@delaplex.in>
2015-11-19 19:09:25 +0530

    Removed system name

--- NSDictionary+NullReplacement.swift
+++ NSDictionary+NullReplacement.swift
@@ -1,9 +1,9 @@
 //
 //  NSDictionary+NullReplacement.swift
-//  Lasso
+//  
 //
-//  Created by QMCPL_MAC2 on 19/11/15.
-//  Copyright (c) 2015 QMC243-MAC-ShrikantP. All rights reserved.
+//  Created by Manish Pathak on 19/11/15.
+//  Copyright (c) 2015 iDevelopers. All rights reserved.
 //
 
 import Foundation
